86a0c2ce7004cb4a3d0d99a0aab5a785aa05932e
iChihebSlaoui <i.chihebslaoui@gmail.com>
2025-09-27 01:30:15 +0200

    flush out decode binary

--- decode_binary.py
+++ decode_binary.py
@@ -10,6 +10,9 @@ def decode_stream(stream):
             break
         value = struct.unpack('<d', chunk)[0]  # little-endian double
         print(value)
+        # i want to send each value to stdout as text
+        # but also flush stdout so that the value is immediately available
+        sys.stdout.flush()
 
 if __name__ == "__main__":
     decode_stream(sys.stdin.buffer)
