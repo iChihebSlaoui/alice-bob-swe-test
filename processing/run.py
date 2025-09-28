8057ee308a306868df82752fffd44fb2aa434351
iChihebSlaoui <i.chihebslaoui@gmail.com>
2025-09-28 13:16:59 +0200

    adding multiprocessing part in pipeline mode

--- processing/run.py
+++ processing/run.py
@@ -5,6 +5,7 @@ import struct
 import sys
 from collections import deque
 from time import sleep
+from concurrent.futures import ProcessPoolExecutor
 
 DOUBLE_SIZE = 8  # 8 bytes for double
 
@@ -24,18 +25,28 @@ class MovingAverage:
 
 
 # streams is a list of tuples (window_length, input_file, output_file)
-def process_streams(streams):
-    print(f"Processing {len(streams)} streams")
-    for win_len, infile, outfile in streams:
-        ma = MovingAverage(win_len)  # Initialize moving average
-        while True:
-            chunk = infile.read(DOUBLE_SIZE)
-            if len(chunk) < DOUBLE_SIZE:
-                print("End of stream")
-                break
-            value = struct.unpack("<d", chunk)[0]  # little-endian double
-            avg_value = ma.next(value)
-            print(avg_value)
+def process_single_stream(args):
+    win_len, infilename, outfilename = args
+    print(f"Processing stream with window length {win_len}, input {infilename}, output {outfilename}")
+    if infilename == "-":
+        infile = sys.stdin.buffer
+    else:
+        infile = open(infilename, "rb")
+    if outfilename == "-":
+        outfile = sys.stdout.buffer
+    else:
+        outfile = open(outfilename, "wb")
+    
+    ma = MovingAverage(win_len)  # Initialize moving average
+    while True:
+        chunk = infile.read(DOUBLE_SIZE)
+        if len(chunk) < DOUBLE_SIZE:
+            break
+        value = struct.unpack("<d", chunk)[0]  # little-endian double
+        avg_value = ma.next(value)
+        outfile.write(struct.pack("<d", avg_value))
+    infile.close()
+    outfile.close()
 
 
 if __name__ == "__main__":
@@ -43,13 +54,7 @@ if __name__ == "__main__":
     sleep(0.01)  # TODO: a bit hacky, wait a bit for the first stream to be available
     for arg in sys.argv[1:]:
         win_len, infilename, outfilename = arg.split(",")
-        if infilename == "-":
-            infile = sys.stdin.buffer
-        else:
-            infile = open(infilename, "rb")
-        if outfilename == "-":
-            outfile = sys.stdout.buffer
-        else:
-            outfile = open(outfilename, "wb")
-        stream_params.append((int(win_len), infile, outfile))
-    process_streams(stream_params)
+        stream_params.append((int(win_len), infilename, outfilename))
+    with ProcessPoolExecutor() as pool:
+        pool.map(process_single_stream, stream_params)
+        pool.shutdown()
